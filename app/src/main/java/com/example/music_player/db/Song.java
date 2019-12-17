75caac2ea0762700c80439b02e0091c12475e62a
tongtonger <1342490381@qq.com>
2019-12-17 21:40:27 +0800

    为所有扫描的音乐点击事件

--- app/src/main/java/com/example/music_player/db/Song.java
+++ app/src/main/java/com/example/music_player/db/Song.java
@@ -5,27 +5,52 @@ import androidx.annotation.Nullable;
 import java.io.Serializable;
 
 public class Song implements Serializable {
-    /**
-     * 歌手
-     */
-    public String singer;
-    /**
-     * 歌曲名
-     */
-    public String song;
-    /**
-     * 歌曲的地址
-     */
-    public String path;
-    /**
-     * 歌曲长度
-     */
-    public int duration;
-    /**
-     * 歌曲的大小
-     */
-    public long size;
 
+    public String singer;
+    public String song;//歌曲名
+    public String path;//歌曲的地址
+    public int duration;//长度
+    public long size;//大小
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getSong() {
+        return song;
+    }
+
+    public void setSong(String song) {
+        this.song = song;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
 
     @Override
     public boolean equals(@Nullable Object obj) {
